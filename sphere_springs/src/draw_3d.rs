0eb3585e7a87a2820f1056f4cca9a5fba29d18a0
Alon Spinner <alonspinner@gmail.com>
2023-10-17 15:12:40 +0300

    making progress

--- sphere_springs/src/draw_3d.rs
+++ sphere_springs/src/draw_3d.rs
@@ -28,8 +28,10 @@ pub fn draw_3d(timestamps : &Vec<f64> ,points : &Vec<Vec<SphericalPoint>>, r : f
     );
     let mut control = OrbitControl::new(*camera.target(), 1.0, 100.0);
 
-    let mut sphere = Gm::new(
-        Mesh::new(&context, &CpuMesh::sphere(32)),
+    let mut mesh = CpuMesh::sphere(32);
+    mesh.transform(&Mat4::from_scale(r)).unwrap();
+    let sphere = Gm::new(
+        Mesh::new(&context, &mesh),
         PhysicalMaterial::new_transparent(
             &context,
             &CpuMaterial {
@@ -37,40 +39,62 @@ pub fn draw_3d(timestamps : &Vec<f64> ,points : &Vec<Vec<SphericalPoint>>, r : f
                     r: 255,
                     g: 0,
                     b: 0,
-                    a: 200,
+                    a: 100,
                 },
                 ..Default::default()
             },
         ),
     );
+
+    let n_points = points[0].len();
+    let mut points = Vec::with_capacity(n_points);
+    for _ in 0..n_points {
+        let mut mesh = CpuMesh::sphere(32);
+        mesh.transform(&Mat4::from_scale(0.1 * r)).unwrap();
+        let mut point = Gm::new(
+            Mesh::new(&context, &mesh),
+            PhysicalMaterial::new_transparent(
+                &context,
+                &CpuMaterial {
+                    albedo: Srgba::BLUE,
+                    ..Default::default()
+                },
+            ),
+        );
+        point.set_animation(|time| {
+            Mat4::from_translation(vec3(time, 0.0, 0.0))
+        });
+        points.push(point)
+        
+    }
+
     // let time_vec_clone = Arc::clone(&time_vec_arc);
     // let theta_vec_clone = Arc::clone(&theta_vec_arc);
-    sphere.set_animation(move |time| {
-        Mat4::from_translation(vec3(0.0, 0.0, 0.0))
-    });
-
     
     //more objects
-    let axes = Axes::new(&context, 0.1, 2.0);
-    let light0 = DirectionalLight::new(&context, 1.0, Srgba::WHITE, &vec3(0.0, -0.5, -0.5));
-    let light1 = DirectionalLight::new(&context, 1.0, Srgba::WHITE, &vec3(0.0, 0.5, 0.5));
+    let axes = Axes::new(&context, 0.05 * r, 0.5 * r);
+    let light0 = DirectionalLight::new(&context, 1.0, Srgba::WHITE,
+                                                         &vec3(0.0, -0.5, -0.5));
+    let light1 = DirectionalLight::new(&context, 1.0, Srgba::WHITE,
+                                                         &vec3(0.0, 0.5, 0.5));
 
 
     let start = std::time::Instant::now();
     window.render_loop(move |mut frame_input| {
         camera.set_viewport(frame_input.viewport);
         control.handle_events(&mut camera, &mut frame_input.events);
-        sphere.animate(start.elapsed().as_secs_f32());
-        // points.animate(start.elapsed().as_secs_f32());
+        for p in &mut points {
+            p.animate(start.elapsed().as_secs_f32());
+        }
 
         frame_input
             .screen()
             .clear(ClearState::color_and_depth(1.0, 1.0, 1.0, 1.0, 1.0))
             .render(
                 &camera,
-                sphere
-                .into_iter()
-                .chain(&axes),
+                sphere.into_iter()
+                .chain(&axes)
+                .chain(points.iter().flatten()),
                 &[&light0, &light1],
             );
 
