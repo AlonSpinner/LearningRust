90a470b923e76c23327fba77d2008e51be11a7c5
Alon Spinner <alonspinner@gmail.com>
2023-10-21 19:37:17 +0300

    something wrong with dampening

--- sphere_springs/src/main.rs
+++ sphere_springs/src/main.rs
@@ -1,4 +1,4 @@
-use sphere_springs::math::{SphericalPoint,RK4,cross,normalize};
+use sphere_springs::math::{SphericalPoint,RK4,cross, dot, normalize};
 use sphere_springs::draw_3d::draw_3d;
 use num::complex:: Complex64;
 
@@ -8,50 +8,89 @@ fn main() {
     const R : f64 = 1.0; // m
     const M : f64 = 1.0; // kg
     const K : f64 = 1.0; // N/m
-    const C : f64 = 3.0; // N/(m/s)
-    const N : usize = 4; // number of masses
+    const C : f64 = 1.0; // N/(m/s)
+    const N : usize = 3; // number of masses
     
     let dt : f64 = 0.001; // seconds
     let max_time : f64 = 10.0 * TAU / (K/M).sqrt();
     let iterations : usize = (max_time / dt) as usize;
 
-    fn free_length(dx : f64, theta0 : f64) -> f64 {
-        (Complex64::new(0.0, dx).exp() * Complex64::new(0.0, theta0).exp()).ln().im
+    fn free_length(angle : f64, angle0 : f64) -> f64 {
+        (Complex64::new(0.0, angle).exp() * Complex64::new(0.0, angle0).exp()).ln().im
     }
 
     // build model
     #[allow(unused_variables)]
     fn f(t : f64, x : &Vec<f64>) -> Vec<f64> {
-        //x - [pitch, theta, pitch_dot, theta_dot]_1, [pitch, theta, pitch_dot, theta_dot]_2, ...
+        //x - [theta, phi, theta_dot, phi_dot]_1, [theta, phi, theta_dot, phi_dot]_2, ...
     
         let mut x_dot: Vec<f64> = vec![0.0; 4*N];
         for i in 0..N {
-            let mut force = [0.0, 0.0];
+            let mut f_theta = 0.0;
+            let mut f_phi = 0.0;
             for j in 0..N {
                 if i == j {continue};
-                //compute force proportional to distance and velocity, in tangen space of x_i
+                //compute f_k
                 let sph_j = SphericalPoint::new(R,x[4*j],x[4*j+1]);
                 let sph_i = SphericalPoint::new(R,x[4*i],x[4*i+1]);
                 let (axis, angle, arc) =  sph_i.axis_angle_arc(&sph_j);
-                let tangent = cross(&axis, &normalize(&sph_i.xyz()));
-                
-                
-                
-                
-                // println!("{:?}", dx);
-                let mut dv = [0.0;2];
-                dv[0] = x[4*j+2] - x[4*i+2];
-                dv[1] = x[4*j+3] - x[4*i+3];
-
-                force[0] = -K * free_length(dx[0], 0.0) + C * dv[0];
-                force[1] = -K * free_length(dx[1], PI) + C * dv[1];
+                let tangent = cross(&axis, &normalize(&sph_i.e_r()));
+                let f_tangent = K * R * free_length(angle, PI);
+                f_theta += f_tangent * dot(&tangent, &sph_i.e_theta());
+                f_phi += f_tangent * dot(&tangent, &sph_i.e_phi());
+                          
+                // //compute f_d
+                // let sph_j = SphericalPoint::new(R,x[4*j+2],x[4*j+3]);
+                // let sph_i = SphericalPoint::new(R,x[4*i+2],x[4*i+3]);
+                // if sph_j != sph_i {
+                //     let (axis, angle, arc) =  sph_i.axis_angle_arc(&sph_j);
+                //     let tangent = cross(&axis, &normalize(&sph_i.e_r()));
+                //     //check if tangent caontains nans
+                //     if tangent[0].is_nan() || tangent[1].is_nan() || tangent[2].is_nan() {
+                //         println!("tangent contains nans");
+                //         println!("axis: {:?}", axis);
+                //         println!("sph_i: {:?}", sph_i);
+                //         println!("sph_j: {:?}", sph_j);
+                //         println!("angle: {}", angle);
+                //         println!("arc: {}", arc);
+                //         println!("tangent: {:?}", tangent);
+                //     }
+                //     let f_tangent = -C * arc;
+                //     f_theta += f_tangent * dot(&tangent, &sph_i.e_theta());
+                //     f_phi += f_tangent * dot(&tangent, &sph_i.e_phi());
+                //     assert!(!f_theta.is_nan());
+                // }
+                f_theta += -C * (x[4*j+2] - x[4*i+2]);
+                f_phi += -C * (x[4*j+3] - x[4*i+3]);
             }
-            let a = [force[0]/M, force[1]/M];
+            let theta = x[4*i];
+            let phi = x[4*i+1];
+            let theta_dot = x[4*i+2];
+            let phi_dot = x[4*i+3];
+
+            let theta_ddot = (f_theta/M + R*phi_dot.powi(2)*theta.sin()*theta.cos())/R;
+            let phi_ddot : f64;
+            if theta.sin() != 0.0 {
+                phi_ddot = (f_phi/M - 2.0*R*theta_dot*phi_dot*theta.cos())/(R * theta.sin());
+            } else {
+                phi_ddot = 0.0;
+            }
+
+            // if theta_ddot.is_infinite() {
+            //     println!("theta_ddot is infinite");
+            //     println!("f_theta: {}", f_theta);
+            //     println!("theta: {}", theta);
+            //     println!("phi: {}", phi);
+            //     println!("theta_dot: {}", theta_dot);
+            //     println!("phi_dot: {}", phi_dot);
+            //     println!("theta_ddot: {}", theta_ddot);
+            //     println!("phi_ddot: {}", phi_ddot);
+            // }
             
-            x_dot[4*i] = x[4*i+2];
-            x_dot[4*i+1] = x[4*i+3];
-            x_dot[4*i+2] = a[0];
-            x_dot[4*i+3] = a[1];
+            x_dot[4*i] = theta_dot;
+            x_dot[4*i+1] = phi_dot;
+            x_dot[4*i+2] = theta_ddot;
+            x_dot[4*i+3] = phi_ddot;
         }
         return x_dot;
     }
@@ -64,7 +103,7 @@ fn main() {
             let tmp = SphericalPoint::new(R, x[4*i], x[4*i+1]).xyz();
             positions.push([tmp[0] as f32, tmp[1] as f32, tmp[2] as f32]);
         }
-        positions        
+        positions       
     }
 
     let rk4 = RK4::new(dt, f);
